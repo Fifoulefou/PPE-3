2e508947ff03126440ba9b561c8104fcaccb799a
CorentinBlot <102357661+CorentinBlot@users.noreply.github.com>
2022-12-07 09:51:29 +0100

    Create acceuil.php

---
+++ acceuil.php
@@ -0,0 +1,17 @@
+# PPE-3
+<!DOCTYPE html>
+<meta charset="utf-8">
+<head>
+    <title>Accueil</title>
+    <h1>Page Accueil</h1>
+    <link rel="stylesheet" href="Accueil.css">
+</head>
+<body>
+    <h2 id="RH">Service Ressource Humaine</h2>
+    <button id="B1" onclick href="choixservice.php" >Commercial</button>
+    <div class="ligne_verticale" ></div>
+    <button id="B2" onclick href="choixservice.php" >Ressource Humaine</button>
+    <h2 id="SC">Service Commercial</h2>
+
+</body>
+</html>
